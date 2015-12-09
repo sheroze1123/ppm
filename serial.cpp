fca2fc1d7e8bcfdb5ccc9d75403184f7edb18119
sheroze1123 <mss385@cornell.edu>
2015-12-09 18:37:40 -0500

    Kinda working serial

--- serial.cpp
+++ serial.cpp
@@ -14,22 +14,22 @@ const double G = 6.6748 * 10e-11; //TODO: Scale everything by G
 int main() {
     // INITIALIZATION ////////////////////////////
     
-    int N=128, N_p = 10;
+    int N=128, N_p = 300;
     double L = 100.0;
     double *rho = (double*) malloc (N * N * sizeof(double));
-    double *E_x = (double*) malloc (N * N * sizeof(double));
-    double *E_y = (double*) malloc (N * N * sizeof(double));
-    fftw_complex *rho_k_x = (fftw_complex*) fftw_malloc (N * N * sizeof(fftw_complex));
-    fftw_complex *rho_k_y = (fftw_complex*) fftw_malloc (N * N * sizeof(fftw_complex));
+    double *phi = (double*) malloc (N * N * sizeof(double));
+    double *a_x = (double*) malloc (N * N * sizeof(double));
+    double *a_y = (double*) malloc (N * N * sizeof(double));
+    fftw_complex *rho_k = (fftw_complex*) fftw_malloc (N * N * sizeof(fftw_complex));
     double *particle_pos = (double*) malloc (N_p * 2 * sizeof(double));
     double *particle_vel = (double*) malloc (N_p * 2 * sizeof(double));
     double *particle_mass = (double*) malloc (N_p * sizeof(double));
-    double delta_t = 0.2;
+    double delta_t = 0.1;
     double delta_d = L/N;
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> pos_dis(10.0, 90.0);
+    uniform_real_distribution<> pos_dis(40.0, 60.0);
     uniform_real_distribution<> mass_dis(0.1, 3.0);
     ofstream fp;
     fp.open("dt0.csv");
@@ -42,10 +42,9 @@ int main() {
         particle_mass[i]    = mass_dis(gen);
     }
     fp.close();
-    
-    fftw_plan rho_plan =  fftw_plan_dft_r2c_2d(N, N, rho, rho_k_y, FFTW_ESTIMATE);
-    fftw_plan E_x_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k_x, E_x, FFTW_ESTIMATE);
-    fftw_plan E_y_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k_y, E_y, FFTW_ESTIMATE);
+
+    fftw_plan rho_plan =  fftw_plan_dft_r2c_2d(N, N, rho, rho_k, FFTW_ESTIMATE);
+    fftw_plan phi_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k, phi, FFTW_ESTIMATE);
 
     // TIME STEP ////////////////////////////////
     
@@ -86,42 +85,36 @@ int main() {
 
                 kx_i = n_i * 2 * M_PI / L; // Spatial frequency in x
                 k_sq = kx_i * kx_i + ky_j * ky_j;
-                // mult = -4 * M_PI / k_sq;
                 mult = 4 * M_PI / k_sq;
-                rho_k_x[j*N+i][0] = rho_k_y[j*N+i][0];
-                rho_k_x[j*N+i][1] = rho_k_y[j*N+i][1];
 
-                rho_k_x[j*N+i][0] *= (kx_i * mult);
-                rho_k_x[j*N+i][1] *= (kx_i * mult);
-                rho_k_y[j*N+i][0] *= (ky_j * mult);
-                rho_k_y[j*N+i][1] *= (ky_j * mult);
-
-                // a = rho_k_x[j*N+i][0] * mult * kx_i;
-                // rho_k_x[j*N+i][0] = - rho_k_x[j*N+i][1] * mult;
-                // rho_k_x[j*N+i][1] = a;
-
-                // a = rho_k_y[j*N+i][0] * mult * ky_j;
-                // rho_k_y[j*N+i][0] = - rho_k_y[j*N+i][1] * mult;
-                // rho_k_y[j*N+i][1] = a;
+                rho_k[j*N+i][0] *= (mult);
+                rho_k[j*N+i][1] *= (mult);
             }
         }
-        fftw_execute(E_x_plan);
-        fftw_execute(E_y_plan);
-        double max = 0, E;
-        for (int i=0; i<N_p; i++) {
-            ind_x =  floor(particle_pos[2*i]/delta_d);
-            ind_y =  floor(particle_pos[2*i+1]/delta_d);
-            E = E_x[ind_y*N + ind_x];
-            if (E > max) {
-                max = E;
-            }
-            E = E_y[ind_y*N + ind_x];
-            if (E > max) {
-                max = E;
-            }
-        }
+        fftw_execute(phi_plan);
+        // double max = 0, E;
+        // for (int i=0; i<N_p; i++) {
+            // ind_x =  floor(particle_pos[2*i]/delta_d);
+            // ind_y =  floor(particle_pos[2*i+1]/delta_d);
+            // E = E_x[ind_y*N + ind_x];
+            // if (E > max) {
+                // max = E;
+            // }
+            // E = E_y[ind_y*N + ind_x];
+            // if (E > max) {
+                // max = E;
+            // }
+        // }
 
         // delta_t = sqrt(L/max);
+
+        // TODO: Edge cases
+        for (int j=1; j<N-1; j++) {
+            for (int i=1; i<N-1; i++) {
+                a_x[j*N + i] = (phi[j*N + i-1] -  phi[j*N + i+1])/(2*N*N);
+                a_y[j*N + i] = (phi[(j-1)*N + i] -  phi[(j+1)*N + i])/(2*N*N);
+            }
+        }
 
         char fname[10];
         sprintf(fname, "dt%d.csv", t);
@@ -130,13 +123,8 @@ int main() {
         for (int i=0; i<N_p; i++) {
             ind_x =  floor(particle_pos[2*i]/delta_d);
             ind_y =  floor(particle_pos[2*i+1]/delta_d);
-            // if (ind_x >= N || ind_x < 0 || ind_y >= N || ind_y < 0) {
-                // fp << particle_pos[2*i] << " " << particle_pos[2*i+1] << endl;
-                // continue;
-            // }
-            cout << "E: " <<  E_x[ind_y*N + ind_x]/(N*N) << " " << E_y[ind_y*N + ind_x]/(N*N) << endl;
-            particle_vel[2*i] += E_x[ind_y*N + ind_x]/ (N*N) * delta_t;
-            particle_vel[2*i+1] += E_y[ind_y*N + ind_x]/ (N*N) * delta_t;
+            particle_vel[2*i] -= a_x[ind_y*N + ind_x] * delta_t;
+            particle_vel[2*i+1] -= a_y[ind_y*N + ind_x] * delta_t;
             particle_pos[2*i] += particle_vel[2*i] * delta_t;
             particle_pos[2*i+1] += particle_vel[2*i+1] * delta_t;
 
@@ -150,15 +138,13 @@ int main() {
     }
 
 
-    fftw_destroy_plan(E_x_plan);
-    fftw_destroy_plan(E_y_plan);
     fftw_destroy_plan(rho_plan);
+    fftw_destroy_plan(phi_plan);
     free(particle_pos);
     free(particle_vel);
     free(particle_mass);
     free(rho);
-    free(E_x);
-    free(E_y);
-    fftw_free(rho_k_x);
-    fftw_free(rho_k_y);
+    free(a_x);
+    free(a_y);
+    fftw_free(rho_k);
 }
