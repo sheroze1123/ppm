c7be1506fb475173a10dd2a4223d8344eb4f5000
sheroze1123 <mss385@cornell.edu>
2015-12-12 15:41:05 -0500

    Cleaned up time step code

--- serial.cpp
+++ serial.cpp
@@ -54,6 +54,74 @@ void compute_accelerations(int N, double* acc_x, double* acc_y, double* phi) {
     }
 }
 
+void update_particles(int N_p, int N, double* particle_pos, double* particle_vel, bool* particle_valid, 
+        double* a_x, double* a_y, double delta_t, double delta_d, double L) {
+
+    int ind_x, ind_y;
+    for (int i=0; i<N_p; i++) {
+        if (particle_valid[i]) {
+            ind_x =  floor(particle_pos[2*i]/delta_d);
+            ind_y =  floor(particle_pos[2*i+1]/delta_d);
+            particle_vel[2*i] -= a_x[ind_y*N + ind_x] * delta_t;
+            particle_vel[2*i+1] -= a_y[ind_y*N + ind_x] * delta_t;
+            particle_pos[2*i] += particle_vel[2*i] * delta_t;
+            particle_pos[2*i+1] += particle_vel[2*i+1] * delta_t;
+
+            if ((particle_pos[2*i] < 0.0) || (particle_pos[2*i] > L) 
+                || (particle_pos[2*i+1] < 0.0) ||  (particle_pos[2*i+1] > L)) {
+                
+                particle_valid[i] = false;
+            }
+        }
+    }
+}
+
+void compute_rho(int N_p, int N, double *rho, double* particle_mass, double* particle_pos, 
+        bool* particle_valid, double delta_d) {
+
+    for (int i=0; i<N*N; i++) {
+        rho[i] = 0.0;
+    }
+    int ind_x, ind_y;
+    for (int i=0; i<N_p; i++) {
+        if (particle_valid[i]) {
+            ind_x =  floor(particle_pos[2*i]/delta_d);
+            ind_y =  floor(particle_pos[2*i+1]/delta_d);
+            rho[ind_y*N + ind_x] += particle_mass[i];
+        }
+    }
+
+    for (int i=0; i<N*N; i++) {
+        rho[i] /= (delta_d * delta_d);
+    }
+}
+
+void compute_phi_k(int N, double L, fftw_complex* rho_k) {
+    
+    double n_i, n_j, kx_i, ky_j, k_sq, mult;
+    for (int j=0; j<N; j++) {
+
+        if (j >= N/2)  n_j = (j - N); // Negative frequency mapping in y
+        else n_j = j;
+
+        ky_j = n_j * 2 * M_PI / L; // Spatial frequency in y
+
+        for (int i=0; i<N; i++) {
+            if (i==0 && j==0) continue;
+
+            if (i >= N/2)  n_i = (i - N); // Negative frequency mapping in x
+            else n_i = i;
+
+            kx_i = n_i * 2 * M_PI / L; // Spatial frequency in x
+            k_sq = kx_i * kx_i + ky_j * ky_j;
+            mult = 4 * M_PI / k_sq;
+
+            rho_k[j*N+i][0] *= (mult);
+            rho_k[j*N+i][1] *= (mult);
+        }
+    }
+}
+
 const char* usage =
     "serial -- Serial N-body simulation using a particle-mesh method\n"
     "Flags:\n"
@@ -100,74 +168,26 @@ int main(int argc, char** argv) {
     Marshaller marshaller("particles.txt", L, N, N_p, particle_mass);
     marshaller.marshal(particle_valid, particle_pos);
 
-    fftw_plan rho_plan =  fftw_plan_dft_r2c_2d(N, N, rho, rho_k, FFTW_ESTIMATE);
-    fftw_plan phi_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k, phi, FFTW_ESTIMATE);
+    fftw_plan rho_plan =  fftw_plan_dft_r2c_2d(N, N, rho, rho_k, FFTW_MEASURE);
+    fftw_plan phi_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k, phi, FFTW_MEASURE);
 
     // TIME STEP ////////////////////////////////
     for (int t=1; t<1000; t++) {
 
-        for (int i=0; i<N*N; i++) {
-            rho[i] = 0.0;
-        }
-        int ind_x, ind_y;
-        for (int i=0; i<N_p; i++) {
-            if (particle_valid[i]) {
-                ind_x =  floor(particle_pos[2*i]/delta_d);
-                ind_y =  floor(particle_pos[2*i+1]/delta_d);
-                rho[ind_y*N + ind_x] += particle_mass[i];
-            }
-        }
-
-        for (int i=0; i<N*N; i++) {
-            rho[i] /= (delta_d * delta_d);
-        }
+        compute_rho(N_p, N, rho, particle_mass, particle_pos, particle_valid, delta_d);
 
         fftw_execute(rho_plan);
 
-        double n_i, n_j, kx_i, ky_j, k_sq, mult, y_mult, a;
-        for (int j=0; j<N; j++) {
+        compute_phi_k(N, L, rho_k);
 
-            if (j >= N/2)  n_j = (j - N); // Negative frequency mapping in y
-            else n_j = j;
-
-            ky_j = n_j * 2 * M_PI / L; // Spatial frequency in y
-
-            for (int i=0; i<N; i++) {
-                if (i==0 && j==0) continue;
-
-                if (i >= N/2)  n_i = (i - N); // Negative frequency mapping in x
-                else n_i = i;
-
-                kx_i = n_i * 2 * M_PI / L; // Spatial frequency in x
-                k_sq = kx_i * kx_i + ky_j * ky_j;
-                mult = 4 * M_PI / k_sq;
-
-                rho_k[j*N+i][0] *= (mult);
-                rho_k[j*N+i][1] *= (mult);
-            }
-        }
         fftw_execute(phi_plan);
 
         // TODO: Scaling delta_t
 
         compute_accelerations(N, a_x, a_y, phi); 
 
-        for (int i=0; i<N_p; i++) {
-            if (particle_valid[i]) {
-                ind_x =  floor(particle_pos[2*i]/delta_d);
-                ind_y =  floor(particle_pos[2*i+1]/delta_d);
-                particle_vel[2*i] -= a_x[ind_y*N + ind_x] * delta_t;
-                particle_vel[2*i+1] -= a_y[ind_y*N + ind_x] * delta_t;
-                particle_pos[2*i] += particle_vel[2*i] * delta_t;
-                particle_pos[2*i+1] += particle_vel[2*i+1] * delta_t;
+        update_particles(N_p, N, particle_pos, particle_vel, particle_valid, a_x, a_y, delta_t, delta_d, L);
 
-                if ((particle_pos[2*i] < 0.0) || (particle_pos[2*i] > L) 
-                    || (particle_pos[2*i+1] < 0.0) ||  (particle_pos[2*i+1] > L)) {
-                    
-                    particle_valid[i] = false;
-                }
-            }
-        }
         marshaller.marshal(particle_valid, particle_pos);
     }
 
