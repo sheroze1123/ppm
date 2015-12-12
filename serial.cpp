e0bef7980c0b00ca3531f7b2e1bf902b229f7579
sheroze1123 <mss385@cornell.edu>
2015-12-12 15:26:02 -0500

    Added the ability for particles to move out of the grid

--- serial.cpp
+++ serial.cpp
@@ -14,6 +14,46 @@ using namespace std;
 
 const double G = 6.6748 * 10e-11; //TODO: Scale everything by G
 
+// Initialized the given particles to with random positions and masses but with zero initial velocity.
+void random_particle_initialization(int N_p, double* p_pos, double* p_vel, double* p_mass, bool* p_valid) {
+
+    random_device rd;
+    mt19937 gen(rd());
+    double pos_min = 40.0, pos_max = 60.0, mass_min = 0.1, mass_max = 3.0;
+    uniform_real_distribution<> pos_dis(pos_min, pos_max);
+    uniform_real_distribution<> mass_dis(mass_min, mass_max);
+
+    for (int i=0; i<N_p; i++) {
+        p_valid[i] = true;
+        p_pos[2*i]   = pos_dis(gen);
+        p_pos[2*i+1] = pos_dis(gen);
+        p_vel[2*i]   = 0.0;
+        p_vel[2*i+1] = 0.0;
+        p_mass[i]    = mass_dis(gen);
+    }
+}
+
+void compute_accelerations(int N, double* acc_x, double* acc_y, double* phi) {
+    for (int j=1; j<N-1; j++) {
+        for (int i=1; i<N-1; i++) {
+            acc_x[j*N + i] = (phi[j*N + i-1] -  phi[j*N + i+1])/(2*N*N);
+            acc_y[j*N + i] = (phi[(j-1)*N + i] -  phi[(j+1)*N + i])/(2*N*N);
+        }
+    }
+    
+    // Left-right edges
+    for (int j=0; j<N; j++) {
+        acc_x[j*N] = -phi[j*N + 1]/(2*N*N);
+        acc_x[j*N+(N-1)] = phi[j*N + N-2]/(2*N*N);
+    }
+
+    // Top-bottom edges
+    for (int i=0; i<N; i++) {
+        acc_x[i] = -phi[N + i]/(2*N*N);
+        acc_x[(N-1)*N + i] = phi[(N-2)*N + i]/(2*N*N);
+    }
+}
+
 const char* usage =
     "serial -- Serial N-body simulation using a particle-mesh method\n"
     "Flags:\n"
@@ -50,43 +90,32 @@ int main(int argc, char** argv) {
     double *phi = (double*) malloc (N * N * sizeof(double));
     double *a_x = (double*) malloc (N * N * sizeof(double));
     double *a_y = (double*) malloc (N * N * sizeof(double));
-    fftw_complex *rho_k = (fftw_complex*) fftw_malloc (N * N * sizeof(fftw_complex));
-    double *particle_pos = (double*) malloc (N_p * 2 * sizeof(double));
-    double *particle_vel = (double*) malloc (N_p * 2 * sizeof(double));
+    bool *particle_valid  = (bool*) malloc (N * N * sizeof(double));
+    double *particle_pos  = (double*) malloc (N_p * 2 * sizeof(double));
+    double *particle_vel  = (double*) malloc (N_p * 2 * sizeof(double));
     double *particle_mass = (double*) malloc (N_p * sizeof(double));
+    fftw_complex *rho_k   = (fftw_complex*) fftw_malloc (N * N * sizeof(fftw_complex));
 
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<> pos_dis(40.0, 60.0);
-    uniform_real_distribution<> mass_dis(0.1, 3.0);
-    for (int i=0; i<N_p; i++) {
-        particle_pos[2*i]   = pos_dis(gen);
-        particle_pos[2*i+1] = pos_dis(gen);
-        particle_vel[2*i]   = 0.0;
-        particle_vel[2*i+1] = 0.0;
-        particle_mass[i]    = mass_dis(gen);
-    }
-
-    // Marshaller marshaller("particles.txt", L, N, N_p, particle_mass);
-    // marshaller.marshal(NULL [>TODO<], particle_pos);
+    random_particle_initialization(N_p, particle_pos, particle_vel, particle_mass, particle_valid);
+    Marshaller marshaller("particles.txt", L, N, N_p, particle_mass);
+    marshaller.marshal(particle_valid, particle_pos);
 
     fftw_plan rho_plan =  fftw_plan_dft_r2c_2d(N, N, rho, rho_k, FFTW_ESTIMATE);
     fftw_plan phi_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k, phi, FFTW_ESTIMATE);
 
     // TIME STEP ////////////////////////////////
-    double time = 0.0;
     for (int t=1; t<1000; t++) {
-
-        time = t*delta_t;
 
         for (int i=0; i<N*N; i++) {
             rho[i] = 0.0;
         }
         int ind_x, ind_y;
         for (int i=0; i<N_p; i++) {
-            ind_x =  floor(particle_pos[2*i]/delta_d);
-            ind_y =  floor(particle_pos[2*i+1]/delta_d);
-            rho[ind_y*N + ind_x] += particle_mass[i];
+            if (particle_valid[i]) {
+                ind_x =  floor(particle_pos[2*i]/delta_d);
+                ind_y =  floor(particle_pos[2*i+1]/delta_d);
+                rho[ind_y*N + ind_x] += particle_mass[i];
+            }
         }
 
         for (int i=0; i<N*N; i++) {
@@ -118,45 +147,28 @@ int main(int argc, char** argv) {
             }
         }
         fftw_execute(phi_plan);
-        // double max = 0, E;
-        // for (int i=0; i<N_p; i++) {
-            // ind_x =  floor(particle_pos[2*i]/delta_d);
-            // ind_y =  floor(particle_pos[2*i+1]/delta_d);
-            // E = E_x[ind_y*N + ind_x];
-            // if (E > max) {
-                // max = E;
-            // }
-            // E = E_y[ind_y*N + ind_x];
-            // if (E > max) {
-                // max = E;
-            // }
-        // }
 
-        // delta_t = sqrt(L/max);
+        // TODO: Scaling delta_t
 
-        // TODO: Edge cases
-        for (int j=1; j<N-1; j++) {
-            for (int i=1; i<N-1; i++) {
-                a_x[j*N + i] = (phi[j*N + i-1] -  phi[j*N + i+1])/(2*N*N);
-                a_y[j*N + i] = (phi[(j-1)*N + i] -  phi[(j+1)*N + i])/(2*N*N);
-            }
-        }
+        compute_accelerations(N, a_x, a_y, phi); 
 
         for (int i=0; i<N_p; i++) {
-            ind_x =  floor(particle_pos[2*i]/delta_d);
-            ind_y =  floor(particle_pos[2*i+1]/delta_d);
-            particle_vel[2*i] -= a_x[ind_y*N + ind_x] * delta_t;
-            particle_vel[2*i+1] -= a_y[ind_y*N + ind_x] * delta_t;
-            particle_pos[2*i] += particle_vel[2*i] * delta_t;
-            particle_pos[2*i+1] += particle_vel[2*i+1] * delta_t;
+            if (particle_valid[i]) {
+                ind_x =  floor(particle_pos[2*i]/delta_d);
+                ind_y =  floor(particle_pos[2*i+1]/delta_d);
+                particle_vel[2*i] -= a_x[ind_y*N + ind_x] * delta_t;
+                particle_vel[2*i+1] -= a_y[ind_y*N + ind_x] * delta_t;
+                particle_pos[2*i] += particle_vel[2*i] * delta_t;
+                particle_pos[2*i+1] += particle_vel[2*i+1] * delta_t;
 
-            // TODO: Particles out of bounds handling
-            if (particle_pos[2*i] < 0.0) particle_pos[2*i] += L;
-            if (particle_pos[2*i] > L) particle_pos[2*i] -= L;
-            if (particle_pos[2*i+1] < 0.0) particle_pos[2*i+1] += L;
-            if (particle_pos[2*i+1] > L) particle_pos[2*i+1] -= L;
+                if ((particle_pos[2*i] < 0.0) || (particle_pos[2*i] > L) 
+                    || (particle_pos[2*i+1] < 0.0) ||  (particle_pos[2*i+1] > L)) {
+                    
+                    particle_valid[i] = false;
+                }
+            }
         }
-        // marshaller.marshal(NULL /* TODO */, particle_pos);
+        marshaller.marshal(particle_valid, particle_pos);
     }
 
 
@@ -168,5 +180,6 @@ int main(int argc, char** argv) {
     free(rho);
     free(a_x);
     free(a_y);
+    free(particle_valid);
     fftw_free(rho_k);
 }
