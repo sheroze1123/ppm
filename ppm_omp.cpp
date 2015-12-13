32ae243b43f46620486b8d698d51f3471dcc688c
sheroze1123 <mss385@cornell.edu>
2015-12-12 19:40:59 -0500

    Strong scaling study for OpenMP

--- ppm_omp.cpp
+++ ppm_omp.cpp
@@ -136,12 +136,60 @@ const char* usage =
     "  - n -- number of grid points of the mesh\n"
     "  - p -- number of particles\n"
     "  - l -- side length of simulation\n"
-    "  - t -- time step (0.1)\n";
+    "  - t -- time step (0.1)\n"
+    "  - s -- number of time steps (1000)\n";
+
+void strong_scaling(int t_steps, double* particle_pos, double* particle_vel, double* particle_mass, 
+        bool* particle_valid, int N_p, int N, double L, double* a_x, double* a_y, double* rho, double* phi,
+        fftw_complex* rho_k, double delta_d, double delta_t) {
+
+    FILE *fp;
+    fp = fopen("strong_scaling.csv", "w+");
+    double t_start, t_end, t_threadrun;
+    int thread_max = 26, i;
+
+    for (i = 1; i <= thread_max; i++) {
+        omp_set_num_threads(i);
+        random_particle_initialization(N_p, particle_pos, particle_vel, particle_mass, particle_valid);
+
+        fftw_plan_with_nthreads(i);
+        fftw_plan rho_plan =  fftw_plan_dft_r2c_2d(N, N, rho, rho_k, FFTW_MEASURE);
+        fftw_plan phi_plan =  fftw_plan_dft_c2r_2d(N, N, rho_k, phi, FFTW_MEASURE);
+
+        t_threadrun = 0.0;
+
+        for (int t=1; t<t_steps; t++) {
+            t_start = omp_get_wtime();
+
+            compute_rho(N_p, N, rho, particle_mass, particle_pos, particle_valid, delta_d);
+
+            fftw_execute(rho_plan);
+
+            compute_phi_k(N, L, rho_k);
+
+            fftw_execute(phi_plan);
+
+            // TODO: Scaling delta_t
+
+            compute_accelerations(N, a_x, a_y, phi); 
+
+            update_particles(N_p, N, particle_pos, particle_vel, particle_valid, a_x, a_y, delta_t, delta_d, L);
+
+
+            t_end = omp_get_wtime();
+            t_threadrun += (t_end - t_start);
+        }
+        fftw_destroy_plan(rho_plan);
+        fftw_destroy_plan(phi_plan);
+        fprintf(fp, "%d, %f\n", i, (t_threadrun / (double)t_steps));
+    } 
+    fclose(fp);
+}
 
 int main(int argc, char** argv) {
     // INITIALIZATION ////////////////////////////
 
-    int N=128, N_p = 300;
+    int N=128, N_p = 300, t_steps=1000;
     double L = 100.0;
     double delta_t = 0.1;
 
@@ -153,7 +201,7 @@ int main(int argc, char** argv) {
 
     // Option processing
     extern char* optarg;
-    const char* optstring = "hp:n:l:t:";
+    const char* optstring = "hp:n:l:t:s:";
     int c;
     while ((c = getopt(argc, argv, optstring)) != -1) {
         switch (c) {
@@ -162,6 +210,7 @@ int main(int argc, char** argv) {
             return -1;
         case 'n': N = atoi(optarg); break;
         case 'p': N_p = atoi(optarg); break;
+        case 's': t_steps = atoi(optarg); break;
         case 'l': L = atof(optarg);  break;
         case 't': delta_t = atof(optarg);  break;
         }
@@ -190,7 +239,7 @@ int main(int argc, char** argv) {
     double average_time_ms = 0.0;
 
     for (int t=1; t<1000; t++) {
-        auto t_start = chrono::high_resolution_clock::now();
+        double t_start = omp_get_wtime();
 
         compute_rho(N_p, N, rho, particle_mass, particle_pos, particle_valid, delta_d);
 
@@ -206,14 +255,16 @@ int main(int argc, char** argv) {
 
         update_particles(N_p, N, particle_pos, particle_vel, particle_valid, a_x, a_y, delta_t, delta_d, L);
 
-        auto t_end = chrono::high_resolution_clock::now();
-        int time_ms = chrono::duration_cast<chrono::milliseconds>(t_end-t_start).count();
-        average_time_ms = ((t-1) * average_time_ms + time_ms)/(double)t;
+        double t_end = omp_get_wtime();
+        average_time_ms += (t_end - t_start);
 
         // marshaller.marshal(particle_valid, particle_pos);
     }
 
-    cout << "Average time per step in milliseconds: " << average_time_ms << endl;
+    cout << "Average time per step in milliseconds: " << average_time_ms/t_steps << endl;
+
+    strong_scaling(t_steps, particle_pos, particle_vel, particle_mass, particle_valid, N_p, N, 
+            L, a_x, a_y, rho, phi, rho_k, delta_d, delta_t);
 
     fftw_destroy_plan(rho_plan);
     fftw_destroy_plan(phi_plan);
